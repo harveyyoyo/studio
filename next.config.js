2e979bd645969047a47c62cf7dfc93176d55e053
Dovid Teitelbaum <sdeichemed@gmail.com>
2026-02-25 02:51:10 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.js
+++ next.config.js
@@ -35,14 +35,7 @@ const nextConfig = {
     ],
   },
   env: {
-    NEXT_PUBLIC_VERSION: `v1.0.6 - ${new Date().toLocaleString('en-US', {
-      year: 'numeric',
-      month: '2-digit',
-      day: '2-digit',
-      hour: '2-digit',
-      minute: '2-digit',
-      hour12: false
-    }).replace(/(\d+)\/(\d+)\/(\d+),/, '$3-$1-$2')}`,
+    NEXT_PUBLIC_VERSION: 'v1.0.6',
   },
 };
 
