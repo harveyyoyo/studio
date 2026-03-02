d9f49dfc9b5011eed5ab1226245a88f82f23c6a7
Dovid Teitelbaum <sdeichemed@gmail.com>
2026-03-02 23:29:12 +0000

    No good

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -18,7 +18,7 @@ export function Logo({ className }: { className?: string }) {
           src={logoImage.imageUrl}
           fill
           alt={logoImage.description}
-          className="object-cover"
+          className="object-contain"
           data-ai-hint={logoImage.imageHint}
           sizes="(max-width: 768px) 32px, 40px"
           priority
