36317a67544e821aae5d9553b2bdf32ec56a034f
Dovid Teitelbaum <sdeichemed@gmail.com>
2026-03-06 10:42:20 +0000

    Is it defaulting to app mode on mobile?

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -3,17 +3,22 @@ import * as React from "react"
 const MOBILE_BREAKPOINT = 768
 
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
+  const [isMobile, setIsMobile] = React.useState(false)
 
   React.useEffect(() => {
-    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
-    const onChange = () => {
+    const checkIsMobile = () => {
       setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
     }
-    mql.addEventListener("change", onChange)
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    return () => mql.removeEventListener("change", onChange)
+    
+    // Initial check on component mount
+    checkIsMobile()
+
+    // Listen for changes
+    window.addEventListener("resize", checkIsMobile)
+
+    // Cleanup
+    return () => window.removeEventListener("resize", checkIsMobile)
   }, [])
 
-  return !!isMobile
+  return isMobile
 }
