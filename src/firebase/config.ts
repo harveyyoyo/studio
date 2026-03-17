6fbc0c5484ead3d915eb22b84f556bf3f4c902a1
Dovid Teitelbaum <sdeichemed@gmail.com>
2026-03-17 16:34:55 +0000

    Fix for Firebase initialization

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   appId: "1:12494403927:web:60c2e4367b55c7921ee612",
   apiKey: "AIzaSyBUH3r37IqZkJ9SmvWaaAJ5HU29Wa_hJLY",
   authDomain: "studio-1273073612-71183.firebaseapp.com",
+  databaseURL: "https://studio-1273073612-71183.firebaseio.com",
   measurementId: "",
   messagingSenderId: "12494403927",
   storageBucket: "studio-1273073612-71183.appspot.com",
